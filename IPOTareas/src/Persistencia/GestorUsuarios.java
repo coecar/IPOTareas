3ea286642c2e07302630784b23c051d0839a818c
coecar <carlos.coello1@alu.uclm.es>
2017-12-05 11:14:51 +0100

    BBDD

--- IPOTareas/src/Persistencia/GestorUsuarios.java
+++ IPOTareas/src/Persistencia/GestorUsuarios.java
@@ -1,74 +1,49 @@
 package Persistencia;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import Dominio.Usuario;
 
 public class GestorUsuarios {
-	private Agente agenteBD;
-	private List<Usuario> usuarios;
 	
-	public GestorUsuarios() {
-		usuarios = new ArrayList<>();
-		agenteBD = Agente.getAgente();
+	static Scanner lectura = new Scanner(System.in);
+	
+	public int leerNumero() throws IOException{
+		File archivo = new File ("./usuarios.txt");
+		FileReader fr = new FileReader(archivo);
+		BufferedReader br = new BufferedReader(fr);
+		
+		int numUsuarios = Integer.parseInt(br.readLine());
+		return numUsuarios;
 	}
 	
-	public Usuario read(String Nick) throws SQLException {
-		String sql = "SELECT * FROM usuarios WHERE Nick = '"+Nick+"';";
-		String Nombre = null, Apellido = null, Password = null;
-		int id=0;
-		Usuario usuario;
-		ResultSet rs;
+	public void leerUsuarios() throws IOException{
+		File archivo = new File ("./usuarios.txt");
+		int numUsuarios = leerNumero();
+		Usuario[] usuarios = new Usuario[numUsuarios];
+		FileReader fr = new FileReader(archivo);
+		BufferedReader br = new BufferedReader(fr);
 		
-		rs = agenteBD.read(sql);
-		
-		if (rs.next()) {
-			Nick = rs.getString("Nick");
-			Nombre = rs.getString("Nombre");
-			Password = rs.getString("Password");
-			Apellido = rs.getString("Apellido");
+		for(int i=0; i<numUsuarios;i++){
+			String Nick = br.readLine();
+			String Pass = br.readLine();
+			String Nombre = br.readLine();
+			String Apellidos = br.readLine();
+			String Fecha = br.readLine();
+			
+			Usuario usuario = new Usuario(Nick,Nombre,Apellidos,Pass,Fecha);
 		}
 		
-		usuario = new Usuario(Nick, Nombre, Apellido,Password);
-		if (usuario.getPassword()==null){
-			return null;
-		} else {
-		return usuario;
-		}
 	}
 	
-	public List<Usuario> readAll() throws SQLException {
-		String sql = "SELECT * FROM usuarios";
-		String Nick = null, Nombre = null, Apellido = null, Password = null;
-		ResultSet rs;
-		
-		rs = agenteBD.read(sql);
-		
-		while(rs.next()) {
-			Nick = rs.getString("Nick");
-			Nombre = rs.getString("Nombre");
-			Apellido = rs.getString("Apellido");
-			Password = rs.getString("Password");
-			usuarios.add(new Usuario(Nick, Nombre, Apellido, Password));
-		}
-		
-		return usuarios;
-	}
-	
-	public int insert(Usuario u) throws SQLException{
-		String sql = "INSERT INTO usuarios(Nick,Nombre,Apellido,Password) VALUES(?,?,?,?)";
-		PreparedStatement pst = agenteBD.getConection().prepareStatement(sql);
-		pst.setString(1, u.getNick());
-		pst.setString(2, u.getNombre());
-		pst.setString(3, u.getApellido());
-		pst.setString(4, u.getPassword());
-		
-		int resultado = agenteBD.insert(pst);
-		return resultado;
-	}
 
 
 }
